ff71259663f6b55b372233ce6401bd125a63756b
msp <info@radialsolutions.co.uk>
2011-09-06 14:38:41 +0100

    Heroku config - Cedar

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ CenatusCms::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true #MSP TODO don't Heroku alteady run "rake assets:precompile" ??
 
   # Generate digests for assets URLs
   config.assets.digest = true
