99286e54fa6910334111d0d43d2993be2374e557
Andi Studer <andi@Babymac.local>
2011-10-10 22:00:49 +0100

    style fixes 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,51 +43,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  #### for use with HTTParty    ####
-  #
-  #def discogs
-  #  hsh = Discogs.get_artist_releases().parsed_response
-  #  releases_short = hsh["resp"]["artist"]["releases"]["release"]
-  #  releases_short.push(hsh["resp"]["artist"]["releases"]["master"])
-  #  puts releases_short[0]["role"]
-  #  puts "ANDI1"
-  #
-  #  puts releases_short
-  #
-  #  releases_short.delete_if { |item| item["role"] != "Main"}
-  #
-  #  #master_releases = hsh["resp"]["artist"]["releases"]["master"].sort! { |a,b| b["year"] <=> a["year"] }
-  #  #master_release_ids = []
-  #  #for master_release in master_releases do
-  #  #  if master_release["role"] == "Main"
-  #  #    hsh_release = Discogs.get_release(master_release["id"])
-  #  #    @releases.push(hsh_release["resp"]["release"])
-  #  #    #master_release_ids <<
-  #  #  end
-  #  #end
-  #
-  #  ##hsh_releases = Discogs.get_release("342751")
-  #  #puts "ANDI1"
-  #  #
-  #  #puts releases_short
-  #  #puts "ANDI2"
-  #  ##puts hsh_releases
-  #
-  #  #.limit(5)
-  #  #@releases += hsh["resp"]["artist"]["releases"]["master"]
-  #  #@releases.sort! { |a,b| b["year"] <=> a["year"] }
-  #
-  #end
-  #
-  ##def discogs
-  ##  hsh = Discogs.get_artist_releases().parsed_response
-  ##  @releases = hsh["resp"]["artist"]["releases"]["release"]
-  ##  @releases += hsh["resp"]["artist"]["releases"]["master"]
-  ##  @releases.sort! { |a,b| b["year"] <=> a["year"] }
-  ##
-  ##end
-
-
   def set_facebook_headers
     @og_title = CenatusCms::Application::SITE_NAME
     @og_type = "website"
@@ -97,24 +52,3 @@ class ApplicationController < ActionController::Base
     @og_admins = CenatusCms::Application::FB_ADMIN
   end
 end
-
-#
-#### for use with HTTParty  ####
-#
-#class Discogs
-#  include HTTParty
-#  base_uri 'api.discogs.com'
-#  headers 'Accept-Encoding' => 'gzip'
-#  format :xml
-#
-#  def self.get_artist_releases()
-#    get('/artist/'+CenatusCms::Application::DISCOGS_NAME, :query => {:releases => 1, :f => 'xml'})
-#  end
-#
-#  def self.get_release(id)
-#    get('/release/'+id, :query => {:f => 'xml'})
-#  end
-#
-#end
-
-
